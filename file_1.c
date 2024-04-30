d91e51bf80e14cc0629c6ddc3fbef1e5b5bfcaa7
Wims2024 <csireesha358@gmail.com>
2024-04-30 22:04:22 +0530

    adding same function from main branch

--- file_1.c
+++ file_1.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
 }
 
